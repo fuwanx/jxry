3a8e838d6235d09474dac0d883adc68be0b3e85e
fuwanx <70800283+fuwanx@users.noreply.github.com>
2021-03-13 23:53:48 +0800

    Update xiaoleCOOKIE.js

--- xiaoleCOOKIE.js
+++ xiaoleCOOKIE.js
@@ -1,8 +1,6 @@
-
-
 //独立COOKIE文件     ck在``里面填写，多账号换行
-let xiaoleurlVal= ``
-let xiaoleheaderVal= ``
+let xiaoleurlVal= `https://minapp.xqrobot.net/user.php?mod=index&client=minapp&version=2.7&site=yyq&user_openid=ofGEO5FkoZK9VrDiFZoFkVxZPlRk&user_lbs=&city=&u=11084`
+let xiaoleheaderVal= `{"Cookie":"PHPSESSID=u8aeobh74ecfne6e6tnurik947","Accept":"*/*","Connection":"keep-alive","Referer":"https://servicewechat.com/wx7d27c26991711f37/127/page-frame.html","Content-Type":"application/json","Host":"minapp.xqrobot.net","User-Agent":"Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.2(0x18000232) NetType/WIFI Language/zh_CN","Accept-Encoding":"gzip, deflate, br","Accept-Language":"zh-cn"}`
 
 
 
@@ -13,6 +11,3 @@ let xiaolecookie = {
 }
 
 module.exports =  xiaolecookie
-  
-
-
