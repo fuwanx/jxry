d7d0718c029455432e992a027e5f3a7fba2ac83f
fuwanx <70800283+fuwanx@users.noreply.github.com>
2021-03-15 20:21:06 +0800

    Update zhimaCOOKIE.js

--- iosrule/zhimaCOOKIE.js
+++ iosrule/zhimaCOOKIE.js
@@ -2,9 +2,13 @@
 
 //独立COOKIE文件     ck在``里面填写，多账号换行
 
-let zhimabodyVal= ``
+let zhimabodyVal= `{"wToken":"","Accept":"*/*","Connection":"keep-alive","Content-Type":"application/x-www-form-urlencoded","Accept-Encoding":"gzip, deflate, br","Host":"api.sxsjyzm.com","Content-Length":"582","User-Agent":"%E8%8A%9D%E5%AB%B2APP/5.1 CFNetwork/1220.1 Darwin/20.3.0","Accept-Language":"zh-cn"}
 
-let zhimatxbodyVal= ``
+`
+
+let zhimatxbodyVal= `appversion=10&desarr=B9UrcK6HqhbGjEpFP3eNKnGZOgPLMKoE0LqUPC7rhuvRBwh5rmsJ85GcyX87%2BNssL1EZSceLvqa7/p1H6OW9MpP5Vvvl4Yq5CQa11M/qiDRTXpLIreGshvXGPjmzxk7Aj9aHbeHKhd0mlkB5LXyKPLI99zy4582cZDSO7K8PWJVxsQT7ew5znR/n0ObJByGx9T5acfk0ZOgPXuXmK9edHIRS6Wg/S8A3d7%2BvsfI4e7XZOMTsYKM/cyyCam5ONQx10fvsOmsWGOSpn/hNI%2Bm/VWqxze6Hoqo8lW7O148TQrqlpo4i3ysobsJAGHmlqSpHpRUAxdEuPs4xXZTUJi68evTno/QVQJWRA6Uz/MRvla98oQJJVvkHr6UnwTLPkgGPfjfE4m6n%2Bn3%2BqSf7jid384vSSubHzCtf5cvCwNKRrTIRz4TNl%2B8G5p4m6vxD7QV5TN8sQpjsOJFG5ydIOM/QvHss5KoBVkPfGKP8m3APHQ22Fad28/CfQO2FEyghELNkx9IR47AMBFo%2BnTSCynX309wWdy6Ba8KRx3J
+
+`
 
 let zhimacookie = {
 
