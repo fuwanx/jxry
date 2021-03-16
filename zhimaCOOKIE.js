593d5fea68cd831931d064e9ec8a257d0b26f789
fuwanx <70800283+fuwanx@users.noreply.github.com>
2021-03-16 08:23:37 +0800

    Update zhimaCOOKIE.js

--- zhimaCOOKIE.js
+++ zhimaCOOKIE.js
@@ -2,7 +2,7 @@
 
 //独立COOKIE文件     ck在``里面填写，多账号换行
 
-let zhimabodyVal= ``
+let zhimabodyVal= `https://h5.sxsjyzm.com/sesameH5/public/sesameLogin/register.html?onlyid=612545154#reloaded `
 
 let zhimatxbodyVal= ``
 
