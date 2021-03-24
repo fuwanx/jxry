c513bd9ae3375727e75da7e531a63928ea6f86bb
fuwanx <70800283+fuwanx@users.noreply.github.com>
2021-03-24 15:45:28 +0800

    Update iboxpayCOOKIE.js

--- iboxpayCOOKIE.js
+++ iboxpayCOOKIE.js
@@ -2,7 +2,10 @@
 
 //独立COOKIE文件     ck在``里面填写，多账号换行
 
-let refreshtokenVal= ``
+let refreshtokenVal= `fa17c67dd3144b9e9bab8b7c87a00bec
+a234170e68b04ee7b8f23475c95396bc
+c4c685a5feb3409f9811cd2ffec3257b
+e1e91d5c507e4946b8c4e87777b540a6`
 
 
 let iboxpaycookie = {
