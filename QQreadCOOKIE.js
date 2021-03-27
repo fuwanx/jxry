fa74a049a792d6f7bf9b3db2d4b43f079177cee6
fuwanx <70800283+fuwanx@users.noreply.github.com>
2021-03-28 07:13:24 +0800

    Update QQreadCOOKIE.js

--- QQreadCOOKIE.js
+++ QQreadCOOKIE.js
@@ -1,5 +1,12 @@
 //独立COOKIE文件     ck在``里面填写，多账号换行
-let QQreadurlVal = ``
+let QQreadurlVal = `https://newios.reader.qq.com/v7_5_7/userinfo?ywkey=ywkpF1JqTWAt&ywguid=50298628
+https://newios.reader.qq.com/v7_5_7/userinfo?ywkey=ywAi1V2peWca&ywguid=397890836
+https://iostgw6.reader.qq.com/v7_5_7/userinfo?ywkey=ywHV8j2YNyrV&ywguid=403592952
+https://iostgw6.reader.qq.com/v7_5_7/userinfo?ywkey=yw5VwfFvUMZq&ywguid=420650227
+https://iostgw6.reader.qq.com/v7_5_7/userinfo?ywkey=ywTm7n358Ch0&ywguid=397561168
+https://newios.reader.qq.com/v7_5_7/userinfo?ywkey=ywM12UrU97wB&ywguid=540091359
+https://newios.reader.qq.com/v7_5_7/userinfo?ywkey=ywjVCXycGAdE&ywguid=494136757
+https://newios.reader.qq.com/v7_5_7/userinfo?ywkey=yw22zxePoVtv&ywguid=152348031`
 
 
 
